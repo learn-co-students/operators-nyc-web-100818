2f20b69f07bda9a9c319f2d7de254e8cd9e150f2
Henry <hanke.liu@gmail.com>
2018-09-27 14:25:27 -0400

    some local error but passed test

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -1,3 +1,6 @@
+require_relative '../lib/operations.rb'
+
+require_relative './spec_helper.rb'
 
 describe "Operations" do
   let(:file_contents) { File.read(File.join(File.dirname(__FILE__), '..', 'lib/operations.rb')) }
@@ -41,5 +44,5 @@ describe "Operations" do
       expect(match).to include("?")
     end
   end
-  
+
 end
